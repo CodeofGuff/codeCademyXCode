037732d5a3239956d84cab3374e2b03a4968ac66
CodeofGuff <guffcode@gmail.com>
2024-06-20 19:08:03 -0700

    Changes

--- Code History/Code History/ContentView.swift
+++ Code History/Code History/ContentView.swift
@@ -11,8 +11,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-            
             Text("Things are changing")
                 .font(Font.custom("Helvetica", size: 36))
             Text("please standby")
@@ -20,66 +18,6 @@ struct ContentView: View {
                 .foregroundStyle(Color.blue)
                 .bold()
         }
-        
-        HStack(alignment: .firstTextBaseline, spacing: 10) {
-            Button(action: {
-                print("boop")
-                }, label: {
-                Text("Click Me")
-                })
-                .foregroundStyle(.white)
-                .font(Font.custom("Helvetica", size: 20))
-                .padding(10)
-                .background(Color.blue)
-            Button(action: {
-                print("boop")
-                }, label: {
-                Text("This is a button!")
-                })
-                .foregroundStyle(Color.white)
-                .font(Font.custom("Ariel", size: 20))
-                .padding(10)
-                .background(Color.blue)
-        }
-        ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
-            VStack {}
-                .frame(width: 225, height: 225)
-                .background(Color.black)
-            VStack {}
-                .frame(width: 155, height: 150)
-                .background(Color.yellow)
-            VStack {}
-                .frame(width: 150, height: 150)
-                .background(Color.blue)
-        
-            VStack {}
-                .frame(width: 75, height: 75)
-                .background(Color.gray)
-    
-        }
-        VStack {
-            ZStack {
-                VStack {}
-                    .frame(width: 50, height: 10)
-                    .background(Color.black)
-            }
-            Text("Above there!")
-            Color.blue
-            Text("Below there!")
-            ZStack {
-                VStack {}
-                    .frame(width: 50, height: 10)
-                    .background(Color.black)
-            }
-        }
-        HStack {
-            Spacer()
-                .frame(width: 200)
-            Text("I'm blue!")
-                .font(.title)
-                .foregroundColor(.blue)
-        }
-
         .padding()
     }
 }
